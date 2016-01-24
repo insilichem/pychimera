3376dc504f64ddc1668b5eb77e4efecb3855e53c
Jaime RG <jaime.rogue@gmail.com>
2016-01-24 23:16:44 +0100

    More PyPI things

--- setup.py
+++ setup.py
@@ -9,27 +9,16 @@ import pychimera
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with open(os.path.join(here, filename), encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-
-long_description = read('README.md')
-
 setup(
     name='pychimera',
     version=pychimera.__version__,
     url='https://github.com/insilichem/pychimera',
+    download_url='https://github.com/insilichem/pychimera/tarball/{}'.format(
+        pychimera.__version__),
     license='LGPL',
     author=pychimera.__author__,
     author_email='jaime.rogue@gmail.com',
     description=pychimera.__doc__.splitlines()[4],
-    long_description=long_description,
     packages=find_packages(),
     include_package_data=True,
     platforms='any',
