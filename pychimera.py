ef99d2fa79dc9b805ff5689c682c86583a331269
Jaime RG <jaime.rogue@gmail.com>
2016-01-24 23:34:46 +0100

    Version 0.0.2

--- pychimera.py
+++ pychimera.py
@@ -17,8 +17,8 @@ import runpy
 import sys
 import subprocess
 
-__author__ = "Jaime Rodríguez-Guerra (@jaimergp)"
-__version_info__ = (0, 0, 1)
+__author__ = "Jaime Rodríguez-Guerra"
+__version_info__ = (0, 0, 2)
 __version__ = '.'.join(map(str, __version_info__))
 
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 
 def read(fname):
