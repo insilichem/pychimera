ec8693a4cf836c152de14a5f26010e083826ec68
Jaime RGP <jaime.rogue@gmail.com>
2017-05-29 16:20:26 +0200

    PR for #4: Fix failed import

--- pychimera/__init__.py
+++ pychimera/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from core import main, patch_environ, enable_chimera, load_chimera, __version__
-from jupyter import chimera_view, enable_chimera_inline
+from .core import main, patch_environ, enable_chimera, load_chimera, __version__
+from .jupyter_utils import chimera_view, enable_chimera_inline
 
 __author__ = "Jaime Rodríguez-Guerra"
 
