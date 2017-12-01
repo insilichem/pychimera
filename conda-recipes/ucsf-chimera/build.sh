801893778cbfcefa59b88819fc53452e134a2880
Jaime RGP <jaime.rogue@gmail.com>
2017-12-01 15:36:14 +0100

    Chimera recipe: don't install in directories with spaces

--- conda-recipes/ucsf-chimera/build.sh
+++ conda-recipes/ucsf-chimera/build.sh
@@ -38,12 +38,12 @@ case "$uname_out" in
       (32)
         _file="chimera-${PKG_VERSION}-win32.exe"
         _filepath="win32/${_file}"
-        _installdir="Chimera ${PKG_VERSION}"
+        _installdir="Chimera_${PKG_VERSION}"
       ;;
       (64)
         _file="chimera-${PKG_VERSION}-win64.exe"
         _filepath="win64/${_file}"
-        _installdir="Chimera ${PKG_VERSION}"
+        _installdir="Chimera_${PKG_VERSION}"
       ;;
     esac
   ;;
