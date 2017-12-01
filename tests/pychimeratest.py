f9f4f8e8c63956e5a73501500ae5c57625391b8d
Jaime RGP <jaime.rogue@gmail.com>
2017-12-01 17:26:28 +0100

    tests: windows cmd does not process wildcards so glob it in Python always

--- tests/pychimeratest.py
+++ tests/pychimeratest.py
@@ -3,10 +3,11 @@
 
 import os
 import sys
+from glob import glob
 import pytest
 from pychimera import patch_environ, enable_chimera
 
 if __name__ == '__main__':
     patch_environ()
     enable_chimera()
-    pytest.main(sys.argv[1:])
+    pytest.main([a for arg in sys.argv[1:] for a in glob(arg)])
