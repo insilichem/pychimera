1d95d064c8c94144bdc1a418eecf8fe8751e7a86
Jaime RGP <jaime.rogue@gmail.com>
2017-12-01 16:07:41 +0100

    tests: universal newlines for cross-platform compat

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,13 +6,16 @@ from subprocess import check_output
 from conftest import datapath
 
 def test_script():
-    out = check_output(['pychimera', datapath('helloworld.py')])
+    out = check_output(['pychimera', datapath('helloworld.py')],
+                       universal_newlines=True)
     assert out == 'Hello world!\n'
 
 def test_c_flag():
-    out = check_output(['pychimera', '-c', 'print("Hello world!")'])
+    out = check_output(['pychimera', '-c', 'print("Hello world!")'],
+                       universal_newlines=True)
     assert out == 'Hello world!\n'
 
 def test_m_flag():
-    out = check_output(['pychimera', '-m', 'helloworld'])
+    out = check_output(['pychimera', '-m', 'helloworld'],
+                       universal_newlines=True)
     assert out == 'Hello world!\n'
